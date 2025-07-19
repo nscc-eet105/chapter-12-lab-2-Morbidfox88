fbaf49aa74fa5f3407047b7049d0621c441383b3
Chad Collard <chadcollard1567@gmail.com>
2025-07-19 14:15:09 -0400

    chapter 12-2 amend

--- Chp12lab2.py
+++ Chp12lab2.py
@@ -7,30 +7,26 @@ print('Unique Word Counter\n')
 
 import os
 
-def unique_word_counter():
-    file_name = ''
+def unique_word_counter(file_name):
     while True:
         if not file_name:
             file_name = input('Enter the name of the file you wish to process: ')
 
-        
         try:
             if not os.path.isfile(file_name):
                 print(f'The file {file_name} could not be found!')
                 
-
-
             with open(file_name, 'r') as file:
                 content = file.read()
                 content = content.lower()
                 words = content.split()
                 unique_words = set(words)
-        
-                print(f'There are {len(unique_words)} unique words in sample.txt\n')
+
+                print(f'There are {len(unique_words)} unique words in {file_name}\n')
                 print('Thanks for using the program!')
                 break
 
-        
+
         except FileNotFoundError:
                 file_name = input('Enter the name of the file you wish to process or type exit to quit ').lower()
                 if file_name == 'exit':
@@ -38,4 +34,4 @@ def unique_word_counter():
                     break
                 else:
                     continue
-unique_word_counter()
+unique_word_counter('')
